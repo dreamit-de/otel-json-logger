0070007c2f0688b7e3b1bef71da277294806c0f0
sgohlke <gohlke.stefan@yahoo.de>
2024-01-23 16:12:05 +0100

    Add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
+        coverage: {
+            exclude: ['build', 'tests'],
+            provider: 'v8',
+            reporter: ['text'],
+        },
         exclude: ['node_modules', 'build', '.idea', '.git', '.cache'],
     },
 })
